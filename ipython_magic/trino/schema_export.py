fc0788ba7d1845e6c27ec486f3b744e45890aceb
cccs-jc <cccs-jc@cyber.gc.ca>
2021-12-03 18:05:33 +0000

    added try/catch around table retrieval
    errors in the catalog prevented the enumeration
    
    try/catch to skip over any errors to retrieve other tables that don't have any errors

--- ipython_magic/trino/schema_export.py
+++ ipython_magic/trino/schema_export.py
@@ -1,46 +1,52 @@
 import json
 import os.path as path
 import time
-
+from trino.exceptions import TrinoUserError
 
 MAX_RET = 20000
 
 def getColumns(cur, tableName):
-    sql = f'SHOW COLUMNS IN {tableName}'
-    #print(sql)
-    cur.execute(sql)
-    rows = cur.fetchmany(MAX_RET)
-    return list(map(lambda r: {
-        'columnName': r[0],
-        'metadata': r[2],
-        'type': r[1],
-        'description': r[3]
-    }, rows))
-
+    try:
+        sql = f'SHOW COLUMNS IN {tableName}'
+        #print(sql)
+        cur.execute(sql)
+        rows = cur.fetchmany(MAX_RET)
+        return list(map(lambda r: {
+            'columnName': r[0],
+            'metadata': r[2],
+            'type': r[1],
+            'description': r[3]
+        }, rows))
+    except TrinoUserError:
+        return []
+        
 
 def getTables(cur, catalog, database):
-    # prevent retrieving tables from information_schema
-    if database == 'information_schema':
+    try:
+        # prevent retrieving tables from information_schema
+        if database == 'information_schema':
+            return []
+
+        path = f'{catalog}.{database}'
+        sql = f'SHOW TABLES IN {path}'
+        #print(sql)
+        cur.execute(sql)
+        rows = cur.fetchmany(MAX_RET)
+        tables = []
+        if len(rows) > 0:
+            for r in rows:
+                if len(tables) > 50:
+                    break
+                table = r[0]
+                tables.append( {
+                    "tableName": table,
+                    "columns": getColumns(cur, f'{path}."{table}"'),
+                    "database": database,
+                    "catalog": catalog
+                })
+        return tables
+    except TrinoUserError:
         return []
-    
-    path = f'{catalog}.{database}'
-    sql = f'SHOW TABLES IN {path}'
-    #print(sql)
-    cur.execute(sql)
-    rows = cur.fetchmany(MAX_RET)
-    tables = []
-    if len(rows) > 0:
-        for r in rows:
-            if len(tables) > 50:
-                break
-            table = r[0]
-            tables.append( {
-                "tableName": table,
-                "columns": getColumns(cur, f'{path}."{table}"'),
-                "database": database,
-                "catalog": catalog
-            })
-    return tables
 
 def getSchemas(cur, catalog):
     sql = f'SHOW SCHEMAS IN {catalog}'
