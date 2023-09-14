47bf0d57e8f651bd22ca24efca600f658b8a11be
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2023-09-14 16:15:19 -0400

    SparkSchemaWidget type check fix

--- jupyterlab_sql_editor/ipython_magic/sparksql/spark_schema_widget.py
+++ jupyterlab_sql_editor/ipython_magic/sparksql/spark_schema_widget.py
@@ -57,62 +57,62 @@ class SparkSchemaWidget(Tree):
     }
 
     def get_children(self, field, name):
-        if isinstance(field, StructField):
+        if type(field) is StructField:
             return self.get_children(field.dataType, field.name)
-        elif isinstance(field, MapType):
+        elif type(field) is MapType:
             key = self.get_children(field.keyType, "key")
             value = self.get_children(field.valueType, "value")
             nodes = [key, value]
             return Node(f"{name}: Map", nodes, icon=icons["map"], **self.complex_type)
-        elif isinstance(field, ArrayType):
+        elif type(field) is ArrayType:
             element = self.get_children(field.elementType, "element")
             nodes = [element]
             return Node(f"{name}: Array", nodes, icon=icons["array"], **self.complex_type)
-        elif isinstance(field, StructType):
+        elif type(field) is StructType:
             nodes = [self.get_children(f, "") for f in field.fields]
             return Node(f"{name}: Struct", nodes, icon=icons["struct"], **self.complex_type)
-        elif isinstance(field, StringType):
+        elif type(field) is StringType:
             return Node(f"{name}: String", icon=icons["string"])
-        elif isinstance(field, TimestampType):
+        elif type(field) is TimestampType:
             return Node(f"{name}: Timestamp", icon=icons["time"])
-        elif isinstance(field, TimestampNTZType):
+        elif type(field) is TimestampNTZType:
             return Node(f"{name}: TimestampNTZ", icon=icons["time"])
-        elif isinstance(field, DateType):
+        elif type(field) is DateType:
             return Node(f"{name}: Date", icon=icons["date"])
-        elif isinstance(field, DayTimeIntervalType):
+        elif type(field) is DayTimeIntervalType:
             return Node(f"{name}: DayTimeInternval", icon=icons["date"])
-        elif isinstance(field, LongType):
+        elif type(field) is LongType:
             return Node(f"{name}: Long", icon=icons["integer"])
-        elif isinstance(field, IntegerType):
+        elif type(field) is IntegerType:
             return Node(f"{name}: Integer", icon=icons["integer"])
-        elif isinstance(field, NullType):
+        elif type(field) is NullType:
             return Node(f"{name}: Null", icon=icons["boolean"])
-        elif isinstance(field, BooleanType):
+        elif type(field) is BooleanType:
             return Node(f"{name}: Boolean", icon=icons["boolean"])
-        elif isinstance(field, AtomicType):
-            return Node(f"{name}: Atomic", icon=icons["boolean"])
-        elif isinstance(field, NumericType):
+        elif type(field) is NumericType:
             return Node(f"{name}: Numeric", icon=icons["decimal"])
-        elif isinstance(field, IntegralType):
-            return Node(f"{name}: Integral", icon=icons["decimal"])
-        elif isinstance(field, DecimalType):
+        elif type(field) is DecimalType:
             return Node(f"{name}: Decimal", icon=icons["decimal"])
-        elif isinstance(field, DoubleType):
+        elif type(field) is DoubleType:
             return Node(f"{name}: Double", icon=icons["decimal"])
-        elif isinstance(field, FloatType):
+        elif type(field) is FloatType:
             return Node(f"{name}: Float", icon=icons["decimal"])
-        elif isinstance(field, FractionalType):
-            return Node(f"{name}: Fractional", icon=icons["decimal"])
-        elif isinstance(field, ShortType):
+        elif type(field) is ShortType:
             return Node(f"{name}: Short", icon=icons["integer"])
+        elif type(field) is BinaryType:
+            return Node(f"{name}: Binary", icon=icons["binary"])
+        elif type(field) is ByteType:
+            return Node(f"{name}: Byte", icon=icons["binary"])
+        elif type(field) is FractionalType:
+            return Node(f"{name}: Fractional", icon=icons["decimal"])
+        elif type(field) is IntegralType:
+            return Node(f"{name}: Integral", icon=icons["decimal"])
+        elif type(field) is AtomicType:
+            return Node(f"{name}: Atomic", icon=icons["boolean"])
+        elif type(field) is UserDefinedType:
+            return Node(f"{name}: UserDefined", icon=icons["binary"])
         elif isinstance(field, DataType):
             return Node(f"{name}: Data", icon=icons["binary"])
-        elif isinstance(field, BinaryType):
-            return Node(f"{name}: Binary", icon=icons["binary"])
-        elif isinstance(field, ByteType):
-            return Node(f"{name}: Byte", icon=icons["binary"])
-        elif isinstance(field, UserDefinedType):
-            return Node(f"{name}: UserDefined", icon=icons["binary"])
 
     def to_tree(self):
         return self
