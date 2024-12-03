f539a4c50d906a0fd3eb479aa0ee7b5614a54862
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2024-12-03 16:23:08 -0500

    improved errors when loading magics doesn't work

--- jupyterlab_sql_editor/ipython_magic/trino/main.py
+++ jupyterlab_sql_editor/ipython_magic/trino/main.py
@@ -15,11 +15,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-
-NODE_LOCATION = shutil.which("node") or shutil.which("node.exe") or shutil.which("node.cmd")
-NODE = str(pathlib.Path(NODE_LOCATION).resolve())
-
-
 mgr = LanguageServerManagerAPI()
 
 # If jupyterlab-lsp has difficulty finding your sql-language-server
@@ -32,18 +27,35 @@ NODE_MODULE = KEY = "sql-language-server"
 SCRIPTS = ["dist", "bin", "cli.js"]
 PATH_TO_BIN_JS = mgr.find_node_module(NODE_MODULE, *SCRIPTS)
 
+try:
+    NODE_LOCATION = shutil.which("node") or shutil.which("node.exe") or shutil.which("node.cmd")
+    NODE = str(pathlib.Path(NODE_LOCATION).resolve())
+    logging.info(f"Node location: {NODE}")
+except Exception:
+    NODE_LOCATION = NODE = ""
+    logging.error("Failed to find the node binary.")
+
+if PATH_TO_BIN_JS is None:
+    logging.error("Failed to find the sql-language-server binary.")
+
 
 def main():
-    logging.info("main function called")
-    logging.info(f"node location: {NODE}")
-    logging.info(f"path to script: {PATH_TO_BIN_JS}")
-    process = subprocess.Popen([NODE, PATH_TO_BIN_JS, *sys.argv[1:]], stdin=sys.stdin, stdout=sys.stdout)
-    logging.info(f"node process started with pid: {process.pid}")
-    sys.exit(process.wait())
+    if NODE and PATH_TO_BIN_JS:
+        logging.info(f"Path to Node: {NODE}. Path to sql-language-server binary: {PATH_TO_BIN_JS}.")
+        process = subprocess.Popen([NODE, PATH_TO_BIN_JS, *sys.argv[1:]], stdin=sys.stdin, stdout=sys.stdout)
+        logging.info(f"Node process started with pid: {process.pid}")
+        sys.exit(process.wait())
 
 
 def load(app):
-    logging.info("load function called")
+    logging.info("trino language server load function called.")
+    if not NODE or not PATH_TO_BIN_JS:
+        raise Exception("Node and/or sql-language-server binary not available.")
+    try:
+        config_schema = json.loads((CONFIG / "{}.schema.json".format(KEY)).read_text(encoding="utf-8"))
+    except Exception as e:
+        logging.error(f"Failed to load config schema: {e}")
+        config_schema = {}
     return {
         "trino-language-server": {
             "version": 2,
@@ -51,7 +63,7 @@ def load(app):
             "languages": ["trino"],
             "display_name": "Trino language server",
             "mime_types": ["application/trino", "text/x-trino"],
-            "config_schema": json.loads((CONFIG / "{}.schema.json".format(KEY)).read_text(encoding="utf-8")),
+            "config_schema": config_schema,
         }
     }
 
